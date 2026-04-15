2b7b04311aadff1c2ac69cc2e5b6428ad15f919b
aldegalts <alinadegalceva@mail.ru>
2026-04-15 20:28:34 +0300

    feat: add BaseRepository

---
+++ app/repositories/base.py
@@ -0,0 +1,34 @@
+from typing import Generic, Sequence, TypeVar
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.base import Base
+
+T = TypeVar("T", bound=Base)
+
+
+class BaseRepository(Generic[T]):
+    model: type[T]
+
+    def __init__(self, db: AsyncSession) -> None:
+        self.db = db
+
+    async def get_by_id(self, id: int) -> T | None:
+        return await self.db.get(self.model, id)
+
+    async def list(self) -> Sequence[T]:
+        result = await self.db.execute(select(self.model))
+        return result.scalars().all()
+
+    def add(self, obj: T) -> None:
+        self.db.add(obj)
+
+    async def delete(self, obj: T) -> None:
+        await self.db.delete(obj)
+
+    async def commit(self) -> None:
+        await self.db.commit()
+
+    async def refresh(self, obj: T, attribute_names: list[str] | None = None) -> None:
+        await self.db.refresh(obj, attribute_names=attribute_names)
